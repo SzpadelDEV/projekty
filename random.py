5045303e96e1f7fa7ba11075f05a1bfd9f578b02
SzpadelDEV <76479125+SzpadelDEV@users.noreply.github.com>
2022-05-17 21:45:08 +0200

    Update random.py

--- random.py
+++ random.py
@@ -1,5 +1,6 @@
 import random ## biblioteki popoga
 
+print("Wylosowana liczba to {} z liczb {}".format(x, y))
 
 print(random.randint(1.22)) ## tu zmmien 1 na inna mniejszą od drugiej a tą 22 zmien na wiekszą niż poprzednią xD
 
