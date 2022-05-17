dd4bb267bfea45c3c7004d229d64181d8e21cec6
SzpadelDEV <76479125+SzpadelDEV@users.noreply.github.com>
2022-05-17 21:37:32 +0200

    Update random.py

--- random.py
+++ random.py
@@ -1,5 +1,5 @@
 import random ## biblioteki popoga
 
 
-print(random.randint(1.22))
+print(random.randint(1.22)) ## tu zmmien 1 na inna mniejszą od drugiej a tą 22 zmien na wiekszą niż poprzednią xD
 
