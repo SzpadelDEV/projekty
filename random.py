5eb5f8ed50dd1d32cf6d667e44b541855525a93f
SzpadelDEV <76479125+SzpadelDEV@users.noreply.github.com>
2022-05-17 21:57:34 +0200

    Update random.py

--- random.py
+++ random.py
@@ -1,6 +1,5 @@
-import random ## biblioteki popoga
-
-print("Wylosowana liczba to {} z liczb {}".format(x, y))
-
-print(random.randint(1.22)) ## tu zmmien 1 na inna mniejszą od drugiej a tą 22 zmien na wiekszą niż poprzednią xD
-
+import random
+x = input("Podaj pierwszą liczbę:\n")
+y = input("\nPodaj drugą liczbę:\n")
+z = random.randint(int(x), int(y))
+print("Wylosowana liczba to {} z zakresu liczb od {} do {}".format(z, x, y))
